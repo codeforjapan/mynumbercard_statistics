20107f04d4267febfbaa0df4744c8d26278a9230
Hal Seki <hal@Hals-MacBook-Pro.local>
2020-07-05 14:02:21 +0900

    add extract_date

--- README.md
+++ README.md
@@ -15,8 +15,20 @@ git clone [this repo]
 docker-compose up -d
 ```
 
-## Get all data
+## When you want to add new libraries
 
 ```
-docker-compose run app pytnon app.py
+pipenv install `library`
+docker-compose up -d --build
+```
+
+## Download csv data from PDF file
+
+```
+docker-compose run app pytnon download.py
+```
+
+If you want to redownload all files
+```
+docker-compose run app pytnon download.py --all
 ```
--- convert_data.py
+++ convert_data.py
@@ -6,7 +6,23 @@ Options:
   -h, --help  show this help message and exit
 """
 import os
+import re
 import json
+from natsort import natsorted
+
+def extract_date(title: str):
+  """extract date object from title string
+
+  Args:
+      title (str): Title string, like マイナンバーカード交付状況（令和2年6月1日現在)
+
+  Returns:
+      date: Date object
+  """
+  match =  re.search(r'[（(](.*)[)）]', title)
+  if (not match):
+    return False
+  return match.groups()[0].replace('現在', '').replace('時点', '')
 
 RAW_DIR = './data/raw'
 OUT_DIR = './data/out'
@@ -20,17 +36,22 @@ if (not os.path.exists(DATA_FILE)):
 
 # load data file
 with open(DATA_FILE) as f:
-  loaded = json.load(f)
+  loaded: dict = json.load(f)
 
 # create dir if it does not exist
 if (not os.path.exists(OUT_DIR)):
   os.makedirs(OUT_DIR)
 
 for key in loaded.keys():
+  date = extract_date(loaded.get(key))
+  if (not date):
+    print('The system could not retrieve date string from the title "{0}" '.format(loaded.get(key)))
+    continue
+  print('Create file for the date {0}'.format(date))
   target_dir = RAW_DIR + '/' + key
   if (not os.path.exists(target_dir)):
     print('The data for the key {0} does not exists. Skip this key'.format(key))
     continue
-  for csv in os.listdir(target_dir):
+  for csv in natsorted(os.listdir(target_dir)):
     print(csv)
     
