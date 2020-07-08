3a26b518e7d3383564949f63374483ae3a2b65dd
Hal Seki <hal@Hals-MacBook-Pro.local>
2020-07-08 11:26:43 +0900

    add StringUtil class

--- convert_data.py
+++ convert_data.py
@@ -11,46 +11,11 @@ import json
 import datetime
 import csv
 import pprint
-from japanera import Japanera, EraDate, Era
+from stringutil import StringUtil
 
 from natsort import natsorted
 
 from converter import Processor, FILETYPE
-      
-def extract_date(title: str):
-  """extract date object from title string
-
-  Args:
-      title (str): Title string, like マイナンバーカード交付状況（令和2年6月1日現在)
-
-  Returns:
-      datetime: Date object
-  """
-  janera = Japanera()
-  match =  re.search(r'[（(](.*)[)）]', title)
-  if (not match):
-    return False
-  datesource = re.search(r'([^0-9元]*)([0-9元]*)年(.*)月(.*)日', match.groups()[0])
-  mydate = janera.strptime('{0}{1}年{2}月{3}日'.format(
-    datesource.groups()[0],
-    datesource.groups()[1].replace('元','1').zfill(2),
-    datesource.groups()[2].zfill(2),
-    datesource.groups()[3].zfill(2)
-  ), "%-E%-o年%m月%d日")
-  return mydate[0]
-
-def to_number(text: str):
-  """return number object if the text is number, otherwise return text
-
-  Args:
-      text (str): text
-  """
-  if (text.replace(',', '').replace('.', '').replace('-', '').replace('%', '').isnumeric()):
-    if ('.' in text):
-      return float(text.replace(',', '').replace('%',''))
-    else:
-      return int(text.replace(',', '').replace('%',''))
-  return text
 
 def delete_total_csvs(outdir: str):
   """delete csv files in the outdir
@@ -87,7 +52,7 @@ delete_total_csvs(OUT_DIR + "/total")
 # load csv data
 for key in loaded.keys():
   processor = Processor() # create processor instance
-  date = extract_date(loaded.get(key))
+  date = StringUtil.extract_date_from_title(loaded.get(key))
   if (not date):
     print('The system could not retrieve date string from the title "{0}" '.format(loaded.get(key)))
     continue
@@ -103,7 +68,7 @@ for key in loaded.keys():
     with open(target_dir + '/' + csvfile) as f:
       reader = csv.reader(f)
       for row in reader:
-        l.append(list(map(to_number, row)))
+        l.append(list(map(StringUtil.to_number, row)))
     processor.appendData(l)
   # save extended data
   ymd = date.strftime('%Y%m%d')
