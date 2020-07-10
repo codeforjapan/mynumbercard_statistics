d75a67cdb3979d48dc57c2d62de29d436cd51df7
Hal Seki <hal@Hals-MacBook-Pro.local>
2020-07-10 22:40:20 +0900

    install flake8 and fix errors

--- converter.py
+++ converter.py
@@ -2,273 +2,296 @@ from enum import Enum
 import os
 import csv
 from stringutil import StringUtil
+
+
 class FILETYPE(Enum):
-      TYPES = 'summary_by_types'
-      DEMOGRAPHIC = 'demographics'
-      PREFECTURES = 'all_prefectures'
-      LOCALGOVS = 'all_localgovs'
+    TYPES = 'summary_by_types'
+    DEMOGRAPHIC = 'demographics'
+    PREFECTURES = 'all_prefectures'
+    LOCALGOVS = 'all_localgovs'
+
 
 class Converter:
-  def __init__(self, list: list):
-    self._list = list
-    self._alllist = []
-    self._ftype = Converter.detectType(list)
+    def __init__(self, list: list):
+        self._list = list
+        self._alllist = []
+        self._ftype = Converter.detectType(list)
 
-  def add_list_column(self, data: list, adddata: list) -> list:
-    """return new list that is added new list of adddata
+    def add_list_column(self, data: list, adddata: list) -> list:
+        """return new list that is added new list of adddata
 
-    Args:
-        data (list): list data which will have new row
-        adddata (list)): list data which will be added to the data
+        Args:
+            data (list): list data which will have new row
+            adddata (list)): list data which will be added to the data
 
-    Returns:
-        list: new list which have new row
-    """
-    ret = []
-    for idx, row in enumerate(data) :
-      ret = ret + [row + adddata[idx]]
-    return ret
-  
-  def add_column(self, header: str, contents):
-    """add new column to the self list
+        Returns:
+            list: new list which have new row
+        """
+        ret = []
+        for idx, row in enumerate(data):
+            ret = ret + [row + adddata[idx]]
+        return ret
 
-    Args:
-        header (str): a header string
-        contents (str, number or list): a contents that should be added. if this is not the list, the data will be added into all rows
-    """
-    if (type(contents) == list):
-      self._list = self.add_list_column(self._list, [header] + contents)
-    else:
-      self._list[0].append(header)
-      map(lambda x: x.append(contents), self._list[1:])
+    def add_column(self, header: str, contents):
+        """add new column to the self list
 
-  def reset(self):
-    """reset alllist instances
-    """
-    self._alllist = []
-  
-  def save_csv(self, path):
-    """save csv file
+        Args:
+            header (str): a header string
+            contents (str, number or list): a contents that should be added.
+            if this is not the list, the data will be added into all rows
+        """
+        if (type(contents) == list):
+            self._list = self.add_list_column(self._list, [header] + contents)
+        else:
+            self._list[0].append(header)
+            map(lambda x: x.append(contents), self._list[1:])
 
-    Args:
-        path (str): path
-    """
-    if (len(self._alllist) == 0):
-      return
-    with open('{0}/{1}.csv'.format(path, self._ftype.value), 'w') as f:
-      writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
-      writer.writerows(self._alllist)
-    print('* saved {0} lines for {1}.csv'.format(len(self._alllist), self._ftype.value))
+    def reset(self):
+        """reset alllist instances
+        """
+        self._alllist = []
 
-  @staticmethod
-  def detectType(l: list) -> FILETYPE:
-    if (l[0][0] == '都道府県名' and l[0][1] == '市区町村名'):
-      return FILETYPE.LOCALGOVS
-    if (l[0][0] == '区分'):
-      return FILETYPE.TYPES
-    if (l[0][0] == '都道府県名' and l[0][1] != '市区町村名'):
-      return FILETYPE.PREFECTURES
-    if (l[0][0] == '年齢'):
-      return FILETYPE.DEMOGRAPHIC
-  @staticmethod
-  def getConverterInstance(list: list):
-    """get Converter instance from the list data
+    def save_csv(self, path):
+        """save csv file
 
-    Args:
-        list (list): list data
+        Args:
+            path (str): path
+        """
+        if (len(self._alllist) == 0):
+            return
+        with open('{0}/{1}.csv'.format(path, self._ftype.value), 'w') as f:
+            writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
+            writer.writerows(self._alllist)
+        print(
+            '* saved {0} lines for {1}.csv'.format(len(self._alllist),
+                                                   self._ftype.value))
 
-    Returns:
-        Converter: an instance of the Converter
-    """
-    if (Converter.detectType(list) == FILETYPE.TYPES):
-      return TypesConverter(list)
-    elif (Converter.detectType(list) == FILETYPE.DEMOGRAPHIC):
-      return DemographicConverter(list)
-    elif (Converter.detectType(list) == FILETYPE.PREFECTURES):
-      return PrefecturesConverter(list)
-    elif (Converter.detectType(list) == FILETYPE.LOCALGOVS):
-      return LocalgovsConverter(list)
-    else:
-      return None
+    @staticmethod
+    def detectType(lst: list) -> FILETYPE:
+        if (lst[0][0] == '都道府県名' and lst[0][1] == '市区町村名'):
+            return FILETYPE.LOCALGOVS
+        if (lst[0][0] == '区分'):
+            return FILETYPE.TYPES
+        if (lst[0][0] == '都道府県名' and lst[0][1] != '市区町村名'):
+            return FILETYPE.PREFECTURES
+        if (lst[0][0] == '年齢'):
+            return FILETYPE.DEMOGRAPHIC
 
-  def convert(self, list: list = None) -> list:
-    """convert data
+    @staticmethod
+    def getConverterInstance(list: list):
+        """get Converter instance from the list data
 
-    Args:
-        list (list, optional): listdata. Defaults to None.
+        Args:
+            list (list): list data
 
-    Returns:
-        list: converted data
-    """
-    return self._convert(list) if list else self._convert(self._list)
+        Returns:
+            Converter: an instance of the Converter
+        """
+        if (Converter.detectType(list) == FILETYPE.TYPES):
+            return TypesConverter(list)
+        elif (Converter.detectType(list) == FILETYPE.DEMOGRAPHIC):
+            return DemographicConverter(list)
+        elif (Converter.detectType(list) == FILETYPE.PREFECTURES):
+            return PrefecturesConverter(list)
+        elif (Converter.detectType(list) == FILETYPE.LOCALGOVS):
+            return LocalgovsConverter(list)
+        else:
+            return None
 
-  def _convert(self, list) -> list:
-    """convet list data, mainly used for subclass (overrided from subclasses)
+    def convert(self, list: list = None) -> list:
+        """convert data
 
-    Args:
-        list ([type]): list of the data
+        Args:
+            list (list, optional): listdata. Defaults to None.
 
-    Returns:
-        list: converted list
-    """
-    return list
+        Returns:
+            list: converted data
+        """
+        return self._convert(list) if list else self._convert(self._list)
 
-  def appendData(self, list: list):
-    """append new data to all array
+    def _convert(self, list) -> list:
+        """convet list data, mainly used for subclass (overrided from subclasses)
 
-    Args:
-        list (list): [description]
-    """
-    self._alllist.extend(self.convert(list))
+        Args:
+            list ([type]): list of the data
+
+        Returns:
+            list: converted list
+        """
+        return list
+
+    def appendData(self, list: list):
+        """append new data to all array
+
+        Args:
+            list (list): [description]
+        """
+        self._alllist.extend(self.convert(list))
+
 
 class Processor:
-  """Processor class is used for process the csv files
-  """
-  def __init__(self):
-    self._converters = []
-
-  def findConverterInstance(self, list: list):
-    """find Converter instance in the local list
-
-    Args:
-        list (list): source data
-
-    Returns:
-        Processor: Processor class instance
+    """Processor class is used for process the csv files
     """
-    converter = Converter.getConverterInstance(list)
 
-    for c in self._converters:
-      if (isinstance(c, type(converter))):
-        return c
-    return None    
+    def __init__(self):
+        self._converters = []
 
-  def appendData(self, list: list):
-    """append list data to the same instance of the converter
+    def findConverterInstance(self, list: list):
+        """find Converter instance in the local list
 
-    Args:
-        list (list): data
-    """
-    converter = self.findConverterInstance(list)
-    if (not converter):
-      converter = Converter.getConverterInstance(list)
-      if (not converter is None):
-        self._converters.append(converter)
+        Args:
+            list (list): source data
 
-    if (not converter is None):
-      converter.appendData(list)
+        Returns:
+            Processor: Processor class instance
+        """
+        converter = Converter.getConverterInstance(list)
 
-  def saveFiles(self, path: str):
-    """save local instance as csv file
+        for c in self._converters:
+            if (isinstance(c, type(converter))):
+                return c
+        return None
 
-    Args:
-        path (str): path
-    """
-    if (not os.path.exists(path)):
-      os.makedirs(path)
-    # save files
-    for c in self._converters:
-      c.save_csv(path)
+    def appendData(self, list: list):
+        """append list data to the same instance of the converter
+
+        Args:
+            list (list): data
+        """
+        converter = self.findConverterInstance(list)
+        if (not converter):
+            converter = Converter.getConverterInstance(list)
+            if (converter is not None):
+                self._converters.append(converter)
+
+        if (converter is not None):
+            converter.appendData(list)
+
+    def saveFiles(self, path: str):
+        """save local instance as csv file
+
+        Args:
+            path (str): path
+        """
+        if (not os.path.exists(path)):
+            os.makedirs(path)
+        # save files
+        for c in self._converters:
+            c.save_csv(path)
 
 # converter child class
+
+
 class TypesConverter(Converter):
-  def _convert(self, _list: list) -> list:
-    """
-    "区分","","人口
-    （H29.1.1時点）","交付枚数
-    （H29.8.31時点）","人口に対する交付枚数率"
-    というヘッダになっているので基準日を抜き出して列として追加する
-    """
-    population_ymd = None
-    card_ymd = None
-    population_date = StringUtil.extract_date_from_header(_list[0][2])
-    if (not population_date is None):
-      population_ymd = population_date.strftime('%Y/%m/%d')
-    card_date = StringUtil.extract_date_from_header(_list[0][3])
-    if (not card_date is None):
-      card_ymd = card_date.strftime('%Y/%m/%d')
-    header = ["区分","", "人口","交付枚数","人口に対する交付枚数率","人口算出基準日","交付件数基準日"]
-    data = list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
-    self._list =  [header] + data
-    return self._list
+    def _convert(self, _list: list) -> list:
+        """
+        "区分","","人口
+        （H29.1.1時点）","交付枚数
+        （H29.8.31時点）","人口に対する交付枚数率"
+        というヘッダになっているので基準日を抜き出して列として追加する
+        """
+        population_ymd = None
+        card_ymd = None
+        population_date = StringUtil.extract_date_from_header(_list[0][2])
+        if (population_date is not None):
+            population_ymd = population_date.strftime('%Y/%m/%d')
+        card_date = StringUtil.extract_date_from_header(_list[0][3])
+        if (card_date is not None):
+            card_ymd = card_date.strftime('%Y/%m/%d')
+        header = ["区分", "", "人口", "交付枚数", "人口に対する交付枚数率", "人口算出基準日", "交付件数基準日"]
+        data = list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
+        self._list = [header] + data
+        return self._list
+
 
 class DemographicConverter(Converter):
-  def _convert(self, _list: list) -> list:
-    print('DemographicConverter')
-    """
+    def _convert(self, _list: list) -> list:
+        print('DemographicConverter')
+        """
     なぜか データの1行目の人口(女)と人口(計)がくっついて閉まっているので分割する
     '65,269,421 127,443,563  11,249,560' というふうになっている
     """
-    fixdata = _list[2][3]
-    
-    if (type(fixdata) is str):
-      _list[2][2] = int(fixdata.split(' ')[0].replace(',',''))
-      _list[2][3] = int(fixdata.split(' ')[1].replace(',',''))
-    """
+        fixdata = _list[2][3]
+
+        if (type(fixdata) is str):
+            _list[2][2] = int(fixdata.split(' ')[0].replace(',', ''))
+            _list[2][3] = int(fixdata.split(' ')[1].replace(',', ''))
+        """
     CSVのヘッダが
     ["年齢","人口（H28.1.1時点）","","","交付件数（H29.5.15時点）","","","交付率","","","全体に対する交付件数割合","",""]
     ["","男","女","計","男","女","計","男","女","計","男","女","計"]
     という2段組になってしまっているので、ヘッダを一行にして、（＊時点）の部分を抜き出して最終列に加える処理を行う
     """
-    population_ymd = StringUtil.extract_date_from_header(self._list[0][1]).strftime('%Y/%m/%d')
-    card_ymd = StringUtil.extract_date_from_header(self._list[0][4]).strftime('%Y/%m/%d')
-    header = [["年齢","人口(男)","人口(女)","人口(計","交付件数(男)","交付件数(女)","交付件数(計)",
-               "交付率(男)","交付率(女)","交付率(計)",
-               "全体に対する交付件数割合(男)","全体に対する交付件数割合(女)","全体に対する交付件数割合(計)",
-               "人口算出基準日","交付件数基準日"]]
-    self._list = [header] + list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
-    return self._list
-class PrefecturesConverter(Converter):
-  def _convert(self, _list):
-    """"
-    ヘッダが
-    "都道府県名","総数（人口）
-    【H28.1.1時点】","交付枚数
-    【H29.5.15時点】","人口に対する
-    交付枚数率"
-    となっているので、日付を抜き出して列に追加する
-    """
-    population_ymd = None
-    card_ymd = None
-    population_date = StringUtil.extract_date_from_header(_list[0][1])
-    if (not population_date is None):
-      population_ymd = population_date.strftime('%Y/%m/%d')
-    card_date = StringUtil.extract_date_from_header(_list[0][2])
-    if (not card_date is None):
-      card_ymd = card_date.strftime('%Y/%m/%d')
-    header = ["都道府県名","総数（人口）","交付枚数","交付率","人口算出基準日","交付件数基準日"]
-    data = list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
-    self._list =  [header] + data
-    return self._list
-  def appendData(self, list: list):
-    if (len(self._alllist) == 0):
-      self._alllist.extend(self.convert(list))
-    else:
-      self._alllist = self.convert(list) + self._alllist[1:]
-class LocalgovsConverter(Converter):
-  def _convert(self, _list):
-    """"
-    ヘッダが
-    都道府県名","市区町村名","総数（人口）
-    【H28.1.1時点】","交付枚数
-    【H29.5.15時点】","交付率"
-    となっているので、日付を抜き出して列に追加する
-    """
-    population_ymd = None
-    card_ymd = None
-    population_date = StringUtil.extract_date_from_header(_list[0][2])
-    if (not population_date is None):
-      population_ymd = population_date.strftime('%Y/%m/%d')
-    card_date = StringUtil.extract_date_from_header(_list[0][3])
-    if (not card_date is None):
-      card_ymd = card_date.strftime('%Y/%m/%d')
-    header = ["都道府県名","市区町村名","総数（人口）","交付枚数","交付率","人口算出基準日","交付件数基準日"]
-    data = list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
-    self._list = [header] + data
-    return self._list
+        population_ymd = StringUtil.extract_date_from_header(
+            self._list[0][1]).strftime('%Y/%m/%d')
+        card_ymd = StringUtil.extract_date_from_header(
+            self._list[0][4]).strftime('%Y/%m/%d')
+        header = [["年齢", "人口(男)", "人口(女)", "人口(計", "交付件数(男)",
+                   "交付件数(女)", "交付件数(計)",
+                   "交付率(男)", "交付率(女)", "交付率(計)",
+                   "全体に対する交付件数割合(男)", "全体に対する交付件数割合(女)", "全体に対する交付件数割合(計)",
+                   "人口算出基準日", "交付件数基準日"]]
+        self._list = [
+            header] + list(map(lambda x: x +
+                               [population_ymd, card_ymd], _list[2:]))
+        return self._list
 
-  def appendData(self, _list: list):
-    if (len(self._alllist) == 0):
-      self._alllist.extend(self.convert(_list))
-    else:
-      self._alllist.extend(self.convert(_list)[2:])
+
+class PrefecturesConverter(Converter):
+    def _convert(self, _list):
+        """"
+        ヘッダが
+        "都道府県名","総数（人口）
+        【H28.1.1時点】","交付枚数
+        【H29.5.15時点】","人口に対する
+        交付枚数率"
+        となっているので、日付を抜き出して列に追加する
+        """
+        population_ymd = None
+        card_ymd = None
+        population_date = StringUtil.extract_date_from_header(_list[0][1])
+        if (population_date is not None):
+            population_ymd = population_date.strftime('%Y/%m/%d')
+        card_date = StringUtil.extract_date_from_header(_list[0][2])
+        if (card_date is not None):
+            card_ymd = card_date.strftime('%Y/%m/%d')
+        header = ["都道府県名", "総数（人口）", "交付枚数", "交付率", "人口算出基準日", "交付件数基準日"]
+        data = list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
+        self._list = [header] + data
+        return self._list
+
+    def appendData(self, list: list):
+        if (len(self._alllist) == 0):
+            self._alllist.extend(self.convert(list))
+        else:
+            self._alllist = self.convert(list) + self._alllist[1:]
+
+
+class LocalgovsConverter(Converter):
+    def _convert(self, _list):
+        """"
+        ヘッダが
+        都道府県名","市区町村名","総数（人口）
+        【H28.1.1時点】","交付枚数
+        【H29.5.15時点】","交付率"
+        となっているので、日付を抜き出して列に追加する
+        """
+        population_ymd = None
+        card_ymd = None
+        population_date = StringUtil.extract_date_from_header(_list[0][2])
+        if (population_date is not None):
+            population_ymd = population_date.strftime('%Y/%m/%d')
+        card_date = StringUtil.extract_date_from_header(_list[0][3])
+        if (card_date is not None):
+            card_ymd = card_date.strftime('%Y/%m/%d')
+        header = ["都道府県名", "市区町村名", "総数（人口）",
+                  "交付枚数", "交付率", "人口算出基準日", "交付件数基準日"]
+        data = list(map(lambda x: x + [population_ymd, card_ymd], _list[2:]))
+        self._list = [header] + data
+        return self._list
+
+    def appendData(self, _list: list):
+        if (len(self._alllist) == 0):
+            self._alllist.extend(self.convert(_list))
+        else:
+            self._alllist.extend(self.convert(_list)[2:])
--- download.py
+++ download.py
@@ -8,33 +8,36 @@ Options:
 """
 import os
 import json
-import pprint
 
 import urllib.request
 import lxml.html
 from docopt import docopt
 import camelot
 
-args = docopt(__doc__) 
+args = docopt(__doc__)
+
 
 def getFileID(filepath: str):
-  return filepath.rsplit('/',1)[1].replace('.pdf','')
+    return filepath.rsplit('/', 1)[1].replace('.pdf', '')
 
 # load PDF file and save table data to csv files
-def loadPDF(filepath: str):
-  print("load: " + filepath)
-  id = getFileID(filepath)
-  tables = camelot.read_pdf(filepath, pages = "1-end")
-  print("Total tables extracted:", tables.n)
-  if (not os.path.exists('./data/raw')):
-    os.makedirs('./data/raw')
-  for idx, table in enumerate(tables):
-    fname = './data/raw/{0}/{1}.csv'.format(id, idx)
-    print(fname)
-    if (not os.path.exists('./data/raw/{0}'.format(id))):
-      os.makedirs('./data/raw/{0}'.format(id))
 
-    table.to_csv(fname)
+
+def loadPDF(filepath: str):
+    print("load: " + filepath)
+    id = getFileID(filepath)
+    tables = camelot.read_pdf(filepath, pages="1-end")
+    print("Total tables extracted:", tables.n)
+    if (not os.path.exists('./data/raw')):
+        os.makedirs('./data/raw')
+    for idx, table in enumerate(tables):
+        fname = './data/raw/{0}/{1}.csv'.format(id, idx)
+        print(fname)
+        if (not os.path.exists('./data/raw/{0}'.format(id))):
+            os.makedirs('./data/raw/{0}'.format(id))
+
+        table.to_csv(fname)
+
 
 # url of the mynumber card PDF
 PDF_URL = "https://www.soumu.go.jp/kojinbango_card/"
@@ -44,8 +47,8 @@ DATA_FILE = "./data/loaded_files.json"
 # load data file
 loaded = {}
 if (not args.get('--all') and os.path.exists(DATA_FILE)):
-  with open(DATA_FILE) as f:
-    loaded = json.load(f)
+    with open(DATA_FILE) as f:
+        loaded = json.load(f)
 
 # download PDF files
 print('DOWNLOAD statistics file')
@@ -56,14 +59,14 @@ result = tree.xpath('//*[@id="contentsWrapper"]/div[2]/div[2]/div[4]/ul/li')
 
 # read PDF links
 for elem in result:
-  link = elem.find('a')
-  id = getFileID(link.get('href'))
-  if (loaded.get(id)):
-    print("skip " + link.text)
-  else:
-    print("get " + link.text)
-    loadPDF(link.get('href'))
-    loaded[id] = link.text
+    link = elem.find('a')
+    id = getFileID(link.get('href'))
+    if (loaded.get(id)):
+        print("skip " + link.text)
+    else:
+        print("get " + link.text)
+        loadPDF(link.get('href'))
+        loaded[id] = link.text
 
 # save loaded files data
 with open(DATA_FILE, 'w', encoding='utf-8') as f:
