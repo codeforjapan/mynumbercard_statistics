12ea2f37813d64f80842b27bd4840f6d99499ad7
halsk <halsk@users.noreply.github.com>
2020-07-24 15:48:01 +0000

    deploy: 6b575bc880b6f2567b311868ff3855c480573ee5

--- component---src-pages-404-tsx-42d6890b0a6d2775c703.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{"i6+/":function(t,n,e){"use strict";e.r(n);e("q1tI");var o=e("Wbzz"),u=e("NqE+"),c=e("TBFr"),d=e("+ego"),l=e("qKvR");n.default=function(){return Object(l.d)(d.a,null,Object(l.d)(u.a,null,Object(l.d)(c.a,null,Object(l.d)("h1",null,"404: Page not found."),Object(l.d)("p",null,"You've hit the void. ",Object(l.d)(o.a,{to:"/"},"Go back.")))))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-42d6890b0a6d2775c703.js.map
