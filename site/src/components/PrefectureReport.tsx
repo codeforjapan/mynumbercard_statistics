6d6a15833f4f97fcf362629c1df3166a39c96ed8
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-05-17 09:49:59 +0900

    change url

--- site/src/components/PrefectureReport.tsx
+++ site/src/components/PrefectureReport.tsx
@@ -6,7 +6,7 @@ const tableauHeight = window.innerWidth <= 600 ? 2050 : 1600
 const SimpleReport: React.FC = () => (
   <div>
     <TableauReport
-      url="https://public.tableau.com/views/mynumber/sheet8?:embed=y&:showVizHome=no&:host_url=https%3A%2F%2Fpublic.tableau.com%2F&:embed_code_version=3&:tabs=no&:toolbar=yes&:animate_transition=yes&:display_static_image=no&:display_spinner=no&:display_overlay=yes&:display_count=yes&:language=en&:loadOrderID=0"
+      url="https://public.tableau.com/views/26886/sheet3?:language=en&:display_count=y&:origin=viz_share_link"
       options={{ width: '100%', height: tableauHeight, class: 'tableau-report' }}
     />
   </div>
