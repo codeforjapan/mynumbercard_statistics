911138a4ccf804f9d6344e5f88bc63e0c75c89c2
Hal Seki <hal@Hals-MacBook-Pro.local>
2021-07-29 21:38:58 +0900

    donwload citycode.csv

--- mynumbercard_data/citycode.py
+++ mynumbercard_data/citycode.py
@@ -4,7 +4,7 @@ import pandas as pd
 class CityCode:
     def __init__(self):
         df_code = pd.read_csv(
-            'https://docs.google.com/spreadsheets/d/e/2PACX-1vR__LlI0QK_dZhhYt-2zDtpYjWqQ38o4lCLlBv8VYUrgKaxKajhacM36F-tlXpt8_WvhyfLTCQ6QFVM/pub?gid=0&single=true&output=csv')
+            './data/citycode.csv')
         df_code["市区町村名"] = df_code["郡名"].fillna("") + df_code["市区町村名"]
         df_code.drop("郡名", axis=1, inplace=True)
         self._df_code = df_code
