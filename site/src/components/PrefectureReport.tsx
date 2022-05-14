35692331c64bb53e0cc52053f75f69a69732192a
Hal Seki <hal@plants-web.jp>
2022-05-14 18:24:53 +0900

    Update PrefectureReport.tsx
    change url

--- site/src/components/PrefectureReport.tsx
+++ site/src/components/PrefectureReport.tsx
@@ -6,7 +6,7 @@ const tableauHeight = window.innerWidth <= 600 ? 2050 : 1600
 const SimpleReport: React.FC = () => (
   <div>
     <TableauReport
-      url="https://public.tableau.com/views/26886/sheet3?:language=en&:display_count=y&:origin=viz_share_link"
+      url="https://public.tableau.com/views/26886/sheet3?:language=en-US&:display_count=y&:origin=viz_share_link"
       options={{ width: '100%', height: tableauHeight, class: 'tableau-report' }}
     />
   </div>
