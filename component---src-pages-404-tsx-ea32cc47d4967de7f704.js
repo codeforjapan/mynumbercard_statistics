9ba11440c12539400b825f9484d1da6a7c24a03c
halsk <halsk@users.noreply.github.com>
2022-05-16 12:25:25 +0000

    deploy: 584f2388f51f711ba9f9468c4af18de0b46688d1

---
+++ component---src-pages-404-tsx-ea32cc47d4967de7f704.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[218],{2224:function(t,u,e){e.r(u);var l=e(1597),n=e(4586),a=e(8653),r=e(7485),s=e(3431);u.default=function(){return(0,s.tZ)(r.Z,{title:"404"},(0,s.tZ)(n.Z,null,(0,s.tZ)(a.Z,null,(0,s.tZ)("h1",null,"404: Page not found."),(0,s.tZ)("p",null,"You've hit the void. ",(0,s.tZ)(l.rU,{to:"/"},"Go back.")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ea32cc47d4967de7f704.js.map
